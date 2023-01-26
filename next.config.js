945c1f80b727af2dc328c5dab43f3075c81fb985
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2023-01-26 12:58:29 +0600

    env and responsive

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: { EDITOR_API: process.env.EDITOR_API },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
