dbd2dd02e3e6ce5903cd4afcc5ba8e243030b7a6
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2023-01-26 13:26:45 +0600

    added no data text

--- src/components/common/LoggedInEle.tsx
+++ src/components/common/LoggedInEle.tsx
@@ -25,12 +25,19 @@ const LoggedInEle = ({ user }: { user: UserProfile }) => {
   return (
     <React.Fragment>
       <SearchBar setSearchKey={setSearchKey} setSortKey={setSortKey} />
+      <div></div>
       <section className="mt-2 px-8 py-4 grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 justify-items-center gap-x-10 gap-y-20 pb-28">
         {isLoading
           ? // eslint-disable-next-line @typescript-eslint/no-unused-vars
             Array.from({ length: 5 }).map((_, index: number) => <CardSkeleton key={index} />)
           : data.data.map((item: NotesData, index: number) => <Note key={index} item={item} />)}
       </section>
+      {data?.data.length === 0 && (
+        <div className="h-full w-full flex justify-center items-center text-center text-xl uppercase font-bold font-[cursive]">
+          you have no notes <br />
+          click the plus button to add new
+        </div>
+      )}
       <div
         onClick={() => setOpen(!open)}
         className="fixed z-20 bg-blue-400 hover:bg-blue-500 text-white bottom-32 right-10 w-12 h-12 rounded-full flex justify-center items-center cursor-pointer transition-colors duration-300"
