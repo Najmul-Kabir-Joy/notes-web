2155f7230889679d51e598054c1f8f391f1c36a5
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2023-01-26 11:27:48 +0600

    rounded skeleton bg updated

--- src/components/loaders/skeletons/RoundedSkeleton.tsx
+++ src/components/loaders/skeletons/RoundedSkeleton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RoundedSkeleton = () => {
   return (
     <div className="animate-pulse">
-      <div className="rounded-full bg-slate-200 h-10 w-10"></div>
+      <div className="rounded-full bg-slate-300 h-10 w-10"></div>
     </div>
   );
 };
